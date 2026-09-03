9a9ccfbb2d75702cbedead5a14eac8276ad0e8e2
HashimTheArab <hashimssoftware@gmail.com>
2026-09-04 02:33:56 +0300

    docs: align coral wall fan water contract

--- server/block/coral_wall_fan.go
+++ server/block/coral_wall_fan.go
@@ -41,7 +41,7 @@ func (c CoralWallFan) NeighbourUpdateTick(pos, _ cube.Pos, tx *world.Tx) {
 	}
 }
 
-// ScheduledTick kills a live fan that is no longer waterlogged or touching water.
+// ScheduledTick kills a live fan that is no longer waterlogged.
 func (c CoralWallFan) ScheduledTick(pos cube.Pos, tx *world.Tx, _ *rand.Rand) {
 	if !coralFanWaterlogged(pos, tx) {
 		c.Dead = true
